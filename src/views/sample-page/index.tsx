6b02a3e0eed5c27fb9281c5725c7b074ae899d8f
Mustakin <mustakin@mpower-social.com>
2024-08-17 11:45:32 +0600

    update resize

--- src/views/sample-page/index.tsx
+++ src/views/sample-page/index.tsx
@@ -2,10 +2,10 @@ import MainCard from 'ui-component/cards/MainCard';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 import { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Xarrow, { useXarrow } from 'react-xarrows';
 import { motion } from 'framer-motion';
+import useWindowSize from '../../hooks/useWindowSize'; // Import the custom hook
 
 interface Position {
     x: number;
@@ -21,16 +21,25 @@ interface PosState {
 }
 
 const SamplePage = () => {
+    const { width } = useWindowSize();
     const updateXarrow = useXarrow();
-    const [posi, setPosi] = useState<PosState>({
-        item1: { x: 0, y: -120 },
-        item2: { x: 120, y: 0 },
+
+    const offset = width < 600 ? 100 : 150; 
+
+    const getInitialPositions = () => ({
+        item1: { x: 0, y: -offset },
+        item2: { x: offset, y: 0 },
         item3: { x: 0, y: 0 },
-        item4: { x: 0, y: 120 },
-        item5: { x: -120, y: 0 }
+        item4: { x: 0, y: offset },
+        item5: { x: -offset, y: 0 }
     });
 
-    const [centerItem, setCenterItem] = useState<keyof PosState>('item3'); // State to track the center item
+    const [posi, setPosi] = useState<PosState>(getInitialPositions);
+    const [centerItem, setCenterItem] = useState<keyof PosState>('item3');
+
+    useEffect(() => {
+        setPosi(getInitialPositions);
+    }, [width]);
 
     const handleDragStop = (itemKey: keyof PosState, e: DraggableEvent, data: DraggableData) => {
         const newPos = { x: data.x, y: data.y };
@@ -44,54 +53,49 @@ const SamplePage = () => {
                     const distX = Math.abs(item.x - newPos.x);
                     const distY = Math.abs(item.y - newPos.y);
 
-                    if (distX <= 50 && distY <= 50) {
-                        // If the overlap is with the center node
+                    if (distX <= offset / 2 && distY <= offset / 2) {
                         if (key === centerItem) {
                             setCenterItem(itemKey);
                             const topKey = Object.keys(prevState).find(
-                                (k) => prevState[k as keyof PosState].x === 0 && prevState[k as keyof PosState].y === -120
+                                (k) => prevState[k as keyof PosState].x === 0 && prevState[k as keyof PosState].y === -offset
                             ) as keyof PosState;
 
                             const bottomKey = Object.keys(prevState).find(
-                                (k) => prevState[k as keyof PosState].x === 0 && prevState[k as keyof PosState].y === 120
+                                (k) => prevState[k as keyof PosState].x === 0 && prevState[k as keyof PosState].y === offset
                             ) as keyof PosState;
 
                             const rightKey = Object.keys(prevState).find(
-                                (k) => prevState[k as keyof PosState].x === 120 && prevState[k as keyof PosState].y === 0
+                                (k) => prevState[k as keyof PosState].x === offset && prevState[k as keyof PosState].y === 0
                             ) as keyof PosState;
 
                             const leftKey = Object.keys(prevState).find(
-                                (k) => prevState[k as keyof PosState].x === -120 && prevState[k as keyof PosState].y === 0
+                                (k) => prevState[k as keyof PosState].x === -offset && prevState[k as keyof PosState].y === 0
                             ) as keyof PosState;
 
                             if (itemKey === topKey) {
                                 newState[itemKey] = { x: 0, y: 0 };
-                                newState[bottomKey] = { x: 0, y: -120 };
-                                newState[key] = { x: 0, y: 120 };
+                                newState[bottomKey] = { x: 0, y: -offset };
+                                newState[key] = { x: 0, y: offset };
                             } else if (itemKey === bottomKey) {
                                 newState[itemKey] = { x: 0, y: 0 };
-                                newState[topKey] = { x: 0, y: 120 };
-                                newState[key] = { x: 0, y: -120 };
+                                newState[topKey] = { x: 0, y: offset };
+                                newState[key] = { x: 0, y: -offset };
                             } else if (itemKey === leftKey) {
                                 newState[itemKey] = { x: 0, y: 0 };
-                                newState[rightKey] = { x: -120, y: 0 };
-                                newState[key] = { x: 120, y: 0 };
+                                newState[rightKey] = { x: -offset, y: 0 };
+                                newState[key] = { x: offset, y: 0 };
                             } else if (itemKey === rightKey) {
                                 newState[itemKey] = { x: 0, y: 0 };
-                                newState[leftKey] = { x: 120, y: 0 };
-                                newState[key] = { x: -120, y: 0 };
+                                newState[leftKey] = { x: offset, y: 0 };
+                                newState[key] = { x: -offset, y: 0 };
                             }
                             return newState;
-                        }
-                        //if center is draged and overlaped with surrounding node
-                        else if (itemKey === centerItem) {
+                        } else if (itemKey === centerItem) {
                             setCenterItem(key as keyof PosState);
                             newState[itemKey as keyof PosState] = { ...item };
                             newState[key as keyof PosState] = { ...prevState[itemKey as keyof PosState] };
                             return newState;
-                        }
-                        //if surrounding node is draged and overlaped with surrounding node
-                        else {
+                        } else {
                             newState[itemKey as keyof PosState] = { ...item };
                             newState[key as keyof PosState] = { ...prevState[itemKey as keyof PosState] };
                             return newState;
@@ -100,12 +104,11 @@ const SamplePage = () => {
                 }
             }
 
-            // Update the position of the dragged item if no overlap
-            //newState[itemKey as keyof PosState] = newPos;
             return newState;
         });
         // setTimeout(updateXarrow, 0);
     };
+
     return (
         <MainCard title="Tree">
             <Container
